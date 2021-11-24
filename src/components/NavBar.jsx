11ff3ed00cfb5a6469349fc5773423dfdaca92ad
luisjulios <antonio.saurith@gmail.com>
2021-11-24 18:37:42 -0300

    Se agrega NavBar y estilos al Header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const navbarStyle = {
     justifyContent: 'center',
     alignItems: 'center',
   }
-function NavBar() {
+const NavBar = () => {
   return (
     <nav style={navbarStyle}>
       <ul style={ulStyle}>
