30db091b864f68f5de2eaa57232dce1bba320887
luisjulios <antonio.saurith@gmail.com>
2022-01-12 17:48:41 -0300

    Se optimiza la logica de consulta de query para las categoruias

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -10,47 +10,40 @@ import {
   where,
 } from "firebase/firestore";
 import "./ItemListContainer.css";
-import { cleanup } from "@testing-library/react";
 
 const ItemListContainer = memo(({ nameCategory, greetings }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
   const { idCategory } = useParams();
 
   useEffect(() => {
     const db = getFirestore();
-    
-    if (idCategory) {
-      const queryProducts = query(
-        collection(db, "productos"),
-        where("category", "==", idCategory)
-      );
+    const queryProducts = idCategory ?
+    query(collection(db, "productos"),where("category", "==", idCategory))
+    :
+    query(collection(db, "productos"));
       getDocs(queryProducts).then((resp) => {
         setProducts(
           resp.docs.map((product) => ({ id: product.id, ...product.data() }))
         );
-        setLoading(false);
-      });
-    } else {
-      const queryProducts = collection(db, "productos");
-      getDocs(queryProducts).then((resp) => {
-        setProducts(
-          resp.docs.map((product) => ({ id: product.id, ...product.data() }))
+        setCategories(
+          resp.docs
+            .map((item) => item.data().category)
+            .reduce(
+              (acc, item) => (acc.includes(item) ? acc : [...acc, item]),
+              []
+            )
         );
         setLoading(false);
       });
-    }
-    cleanup();
   }, [idCategory]);
 
-  if (idCategory === "gatos") {
-    nameCategory = "Gatos";
-  } else if (idCategory === "perros") {
-    nameCategory = "Perros";
-  }
+  const category = categories.filter((category) => category === idCategory);
+
   return (
     <main>
-      <h2>{nameCategory || greetings}</h2>
+      <h2>{greetings || category}</h2>
       {loading ? (
         <img src={Loading} alt="Cargando" className="loading" />
       ) : (
